519977737fb640e2c300daaa85080a6339e17d6a
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-08-19 10:20:45 +0200

    feat: convert to hybrid package

---
+++ .github/test-scripts/test.cjs
@@ -0,0 +1,4 @@
+const PCancelable = require("@esm2cjs/p-cancelable").default;
+const assert = require("assert");
+
+assert(typeof PCancelable === "function");
---
+++ .github/test-scripts/test.mjs
@@ -0,0 +1,4 @@
+import PCancelable from "@esm2cjs/p-cancelable";
+import assert from "assert";
+
+assert(typeof PCancelable === "function");
